cc70407412837a82065588a5115dbb22eac7ea4d
Nicolas Perez <nico__1993@hotmail.com>
2016-04-16 18:15:56 -0300

    Libreria Interna

---
+++ librery/librery.h
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : librery.h
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#ifndef CREAR_CONEXION_CON SERVIDOR
+#define CREAR_CONEXION_CON SERVIDOR
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
