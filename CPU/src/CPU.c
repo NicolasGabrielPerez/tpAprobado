ff3bdd1be94185c6b339d30c97950bb1123644d5
Agustin <aguss.vazquez@gmail.com>
2016-04-18 20:50:17 -0300

    no message

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -11,7 +11,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+#include <commons/log.h>
+#include <commons/collections/list.h>
+
+#include "ansiop.h"
+
+static const char* DEFINICION_VARIABLES = "variables a, b, c";
+static const char* ASIGNACION = "a = b + 12";
+static const char* IMPRIMIR = "print b";
+static const char* IMPRIMIR_TEXTO = "textPrint foo\n";
+
+
+AnSISOP_funciones functions = {
+	.AnSISOP_definirVariable	= dummy_definirVariable,
+	.AnSISOP_obtenerPosicionVariable= dummy_obtenerPosicionVariable,
+	.AnSISOP_dereferenciar	= dummy_dereferenciar,
+	.AnSISOP_asignar	= dummy_asignar,
+	.AnSISOP_imprimir	= dummy_imprimir,
+	.AnSISOP_imprimirTexto	= dummy_imprimirTexto,
+};
+AnSISOP_kernel kernel_functions = { };
+
+
+void correrDefinirVariables() {
+	printf("Ejecutando '%s'\n", DEFINICION_VARIABLES);
+	analizadorLinea(strdup(DEFINICION_VARIABLES), &functions, &kernel_functions);
+	printf("================\n");
+}
+
+
+void correrAsignar() {
+	printf("Ejecutando '%s'\n", ASIGNACION);
+	analizadorLinea(strdup(ASIGNACION), &functions, &kernel_functions);
+	printf("================\n");
+}
+
+
+void correrImprimir() {
+	printf("Ejecutando '%s'\n", IMPRIMIR);
+	analizadorLinea(strdup(IMPRIMIR), &functions, &kernel_functions);
+	printf("================\n");
+}
+
+
+
+void correrImprimirTexto() {
+	printf("Ejecutando '%s'", IMPRIMIR_TEXTO);
+	analizadorLinea(strdup(IMPRIMIR_TEXTO), &functions, &kernel_functions);
+	printf("================\n");
+}
+
+
+
+
+int main(int argc, char **argv) {
+	correrDefinirVariables();
+	correrAsignar();
+	correrImprimir();
+	correrImprimirTexto();
+	return 0;
 }
