dc2306d71b7650f6abf6f6ee54ca8732ded9326b
martinvergara26 <martin.vergara@tictapps.com>
2016-04-18 17:58:35 -0300

    Primer commit

--- UCM/src/UCM.c
+++ UCM/src/UCM.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("#primerpush #utn #so #aymama"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
