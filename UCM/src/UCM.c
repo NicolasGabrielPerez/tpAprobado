75b670a6b9378ff41c42a3bd7740ea52ae252c75
martinvergara26 <martin.vergara@tictapps.com>
2016-04-19 16:41:01 -0300

    Subo archivo de configuracion, y su lectura

--- UCM/src/UCM.c
+++ UCM/src/UCM.c
@@ -10,8 +10,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
 
 int main(void) {
-	printf("#primerpush #utn #so #aymama"); /* prints !!!Hello World!!! */
+	t_config* config;
+	config = config_create("umc.config");
+    int puerto = config_get_int_value(config, "PUERTO");
+    int ip_swap = config_get_int_value(config, "IP_SWAP");
+    int puerto_swap = config_get_int_value(config, "PUERTO_SWAP");
+    int cantdiad_de_marcos = config_get_int_value(config, "MARCOS");
+    int marco_size = config_get_int_value(config, "MARCO_SIZE");
+    int ip_marcos_x_proc = config_get_int_value(config, "MARCOS_X_PROC");
+    int cantidad_entradas_tlb = config_get_int_value(config, "ENTRADAS_TLB");
+    int retardo = config_get_int_value(config, "RETARDO");
+
+    printf("Puerto: :%d", puerto);
 	return EXIT_SUCCESS;
 }
