6089e9a50c70bbe615b825365bf1a267375ad315
martinvergara26 <martin.vergara@tictapps.com>
2016-05-15 21:05:52 -0300

    Uso de shared library de sockets

--- cpu/src/CPU.c
+++ cpu/src/CPU.c
@@ -10,11 +10,12 @@
 #include <parser/metadata_program.h>
 #include <parser/parser.h>
 #include <parser/sintax.h>
+#include <commons/config.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
+#include <sockets/sockets.h>
 
 #include "ansiop.h"
-#include "configInit.h"
 
 static const char* DEFINICION_VARIABLES = "variables a, b, c";
 static const char* ASIGNACION = "a = b + 12";
@@ -72,93 +73,6 @@ void correrImprimirTexto() {
 	printf("================\n");
 }
 
-int crear_socket_cliente(char* ip, char* port) { //devuelve un nuevo socket para conectarse al server especificado
-	int sockfd; //aca se va a poner el socket obtenido mediante getaddrinfo
-
-	struct addrinfo hints; //estructura conf info necesaria para getaddrinfo
-	struct addrinfo *servinfo; //aca se va a poner la info del server
-	struct addrinfo *p; //puntero usado para loopear los
-	char s[INET6_ADDRSTRLEN]; //esto es para el nombre del server al que nos conectamos
-	int rv; //valor que se usa para obtener especificacion de error, en caso de haberlo en getaddrinfo
-
-	memset(&hints, 0, sizeof hints); //se asegura de 'limpiar' la memoria
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if ((getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
-	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-	return 1;
-	}
-
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
-
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) { //hubo error...
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
-
-	//se obtiene y se muestra el nombre del server
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-		s, sizeof s);
-	printf("client: connecting to %s\n", s);
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	return sockfd;
-}
-
-void sendMessage() {
-	char package[PACKAGESIZE];
-	int enviar = 1;
-
-	while(enviar){
-			fgets(package, PACKAGESIZE, stdin);
-			printf("CPU: Mensaje Recibido\n");
-			if (!strcmp(package,"exit\n")) enviar = 0;
-			if (enviar) {
-				send(socketNucleo, package, PACKAGESIZE, 0);
-				//send(socketNucleo, package, strlen(package) + 1, 0);
-			}
-	}
-}
-
-int handshake(int sockfd){
-	char buf[50];
-	int numbytes; //lo uso para poner la cantidad de bytes recibidos
-	puts("CPU: Voy a enviar algo...\n");
-	if (send(sockfd,"Soy CPU    ", 11, 0) == -1) {
-	  perror("send");
-	}
-
-	if ((numbytes = recv(sockfd, buf, 49, 0)) == -1) {
-		perror("recv");
-		exit(1);
-	}
-
-	printf("client: received '%s'\n",buf);
-	printf("numbytes: '%d'\n",numbytes);
-	buf[numbytes] = '\0';
-
-	puts("CPU: handshake finalizado felizmente\n");
-
-	return 0;
-}
-
 int main(int argc, char **argv) {
 	//correrDefinirVariables();
 	//correrAsignar();
@@ -190,12 +104,12 @@ int main(int argc, char **argv) {
 	printf("NUCLEO FD: %d\n", socket_nucleo);
 
 	//Hago handshake con umc
-	if(handshake(socket_umc) != 0){
+	if(handshake(socket_umc, "PRUEBA") != 0){
 		puts("Error en handshake con la umc");
 	}
 
 	//Hago handskae con nucleo
-	if(handshake(socket_nucleo) != 0){
+	if(handshake(socket_nucleo, "PRUEBA") != 0){
 		puts("Error en handshake con la umc");
 	}
 
