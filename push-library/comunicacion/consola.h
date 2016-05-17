75037f027482733c0e6e8b55e85d7c4378ae651f
martinvergara26 <martin.vergara@tictapps.com>
2016-05-16 21:16:17 -0300

    Libreria para enviar y recibir pedidos de programas

---
+++ push-library/comunicacion/consola.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2012 Sistemas Operativos - UTN FRBA. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+	/**
+	* @NAME: enviarSolicitudDeProgramaNuevo
+	* @DESC: devuelve 0 si OK
+	*/
+
+uint32_t enviarSolicitudDeProgramaNuevo();
+
+#endif /* CONSOLA_H_ */
