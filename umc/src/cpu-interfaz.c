60abd21416897fe99aced295034b87413e1ff17a
martinvergara26 <martin.vergara@tictapps.com>
2016-06-01 20:42:26 -0300

    Comunicacion UMC-CPU con response

--- umc/src/cpu-interfaz.c
+++ umc/src/cpu-interfaz.c
@@ -6,7 +6,6 @@ void recibirAlmacenarPaginas(int cpu_socket, int pidActivo){
 	int32_t offset;
 	int32_t tamanio;
 	char* buffer;
-	char* respuesta;
 
 	if (recv(cpu_socket, &nroPagina, sizeof(int32_t), 0) == -1) {
 		perror("recv");
@@ -29,49 +28,50 @@ void recibirAlmacenarPaginas(int cpu_socket, int pidActivo){
 		exit(1);
 	}
 
+	tabla_de_paginas* tablaDePaginas = buscarPorPID(pidActivo);
+	if(tablaDePaginas==NULL){
+		enviarFAIL(cpu_socket, PID_NO_EXISTE);
+		return;
+	}
+
 	if(TLBEnable){
-		//Buscar en TLB. Si esta, ir a buscar a memoria y enviar bytes
 		int frame = buscarEnTLB(nroPagina, pidActivo);
 		if(frame!=-1){
 			escribirEnFrame(buffer, offset, tamanio, frame);
-			respuesta = string_itoa(RESPUESTA_OK);
-			goto enviarBytes;
+			enviarOKSinContenido(cpu_socket);
+			return;
 		}
 	}
 
-	// else: buscar en Tabla de Paginas del pid
+	//buscar en Tabla de Paginas del pid
 	tabla_de_paginas_entry* entrada = buscarPorNroPaginaYPID(nroPagina, pidActivo);
 	if(entrada==NULL){
-		respuesta = string_itoa(RESPUESTA_FAIL); //no existe tal nroPagina para tal pid
-		goto enviarBytes;
+		enviarFAIL(cpu_socket, PAGINA_NO_EXISTE);
 	}
 	if(entrada->presente){
 		// esta en tabla de paginas ⇒ ir a buscar a Memoria Principal
 		escribirEnFrame(buffer, offset, tamanio, entrada->nroFrame);
+		entrada->modificado = 1;
 		// y actualizar TLB
 		if(TLBEnable) actualizarTLB(nroPagina, pidActivo);
 	}
 
 	// no esta ⇒ pedir a Swap
-	char* paginaSolicitada = pedirPaginaASwap(nroPagina, pidActivo);
-	if(paginaSolicitada==NULL){
-		respuesta = string_itoa(RESPUESTA_FAIL); //no existe tal nroPagina para tal pid
-		goto enviarBytes;
+	response* paginaSolicitadaResult = pedirPaginaASwap(nroPagina, pidActivo);
+	if(!paginaSolicitadaResult->ok){ //hubo error
+		enviarResultado(paginaSolicitadaResult, cpu_socket);
+		return;
 	}
+
 	// TODO cargar pagina y actualizar tabla de paginas
 	// reemplazar si es necesario (Clock y clock modificado)
-	cargarPagina(nroPagina, pidActivo, paginaSolicitada);
+	cargarPagina(nroPagina, pidActivo, paginaSolicitadaResult->contenido);
 	escribirEnFrame(buffer, offset, tamanio, entrada->nroFrame);
-	respuesta = string_itoa(RESPUESTA_OK);
 
 	//Actualizar TLB
 	if(TLBEnable) actualizarTLB(nroPagina, pidActivo);
 
-	enviarBytes:
-		if (send(cpu_socket, respuesta, tamanio, 0) == -1) {
-				perror("send");
-				exit(1);
-		}
+	enviarOKSinContenido(cpu_socket);
 }
 
 void recibirSolicitarPaginas(int cpu_socket, int pidActivo){
@@ -95,50 +95,52 @@ void recibirSolicitarPaginas(int cpu_socket, int pidActivo){
 		exit(1);
 	}
 
+	tabla_de_paginas* tablaDePaginas = buscarPorPID(pidActivo);
+	if(tablaDePaginas==NULL){
+		enviarFAIL(cpu_socket, PID_NO_EXISTE);
+		return;
+	}
+
 	if(TLBEnable){
 		//Buscar en TLB. Si esta, ir a buscar a memoria y enviar bytes
 		int frame = buscarEnTLB(nroPagina, pidActivo);
 		if(frame!=-1){
 			bytesAEnviar = obtenerBytesDeMemoriaPrincipal(frame, offset, tamanio);
-			goto enviarBytes;
+			enviarOKConContenido(cpu_socket, tamanio, bytesAEnviar);
+			return;
 		}
 	}
 
 	// else: buscar en Tabla de Paginas del pid
-	tabla_de_paginas_entry* entrada = buscarPorNroPaginaYPID(nroPagina, pidActivo);
+	tabla_de_paginas_entry* entrada = buscarPorNroPaginaYPID(nroPagina, pidActivo); //esta busqueda seria penalizada
 	if(entrada==NULL){
-		bytesAEnviar = string_itoa(RESPUESTA_FAIL); //no existe tal nroPagina para tal pid
-		goto enviarBytes;
+		enviarFAIL(cpu_socket, PAGINA_NO_EXISTE);
+		return;
 	}
+
 	if(entrada->presente){
 		// esta en tabla de paginas ⇒ ir a buscar a Memoria Principal
 		bytesAEnviar = obtenerBytesDeMemoriaPrincipal(entrada->nroFrame, offset, tamanio);
-		// y actualizar TLB
 		if(TLBEnable) actualizarTLB(nroPagina, pidActivo);
+		enviarOKConContenido(cpu_socket, tamanio, bytesAEnviar);
+		return;
 	}
 
 	// no esta ⇒ pedir a Swap
-	char* paginaSolicitada = pedirPaginaASwap(nroPagina, pidActivo);
-	if(paginaSolicitada==NULL){
-		bytesAEnviar = string_itoa(RESPUESTA_FAIL); //no existe tal nroPagina para tal pid
-		goto enviarBytes;
+	response* paginaSolicitadaResult = pedirPaginaASwap(nroPagina, pidActivo);
+	if(!paginaSolicitadaResult->ok){ //hubo error
+		enviarResultado(paginaSolicitadaResult, cpu_socket);
+		return;
 	}
-
-	//TODO Primero enviar OK
 
 	// TODO cargar pagina y actualizar tabla de paginas
 	// reemplazar si es necesario (Clock y clock modificado)
-	cargarPagina(nroPagina, pidActivo, paginaSolicitada);
-	bytesAEnviar = obtenerBytes(paginaSolicitada, offset, tamanio);
-
+	cargarPagina(nroPagina, pidActivo, paginaSolicitadaResult->contenido);
+	bytesAEnviar = obtenerBytes(paginaSolicitadaResult->contenido, offset, tamanio);
 	//Actualizar TLB
 	if(TLBEnable) actualizarTLB(nroPagina, pidActivo);
 
-	enviarBytes:
-		if (send(cpu_socket, bytesAEnviar, tamanio, 0) == -1) {
-				perror("send");
-				exit(1);
-		}
+	enviarOKConContenido(cpu_socket, paginaSolicitadaResult->contenidoSize, paginaSolicitadaResult->contenido);
 }
 
 void recibirCambioDeProcesoActivo(int cpu_socket, int* pidActivo){
@@ -146,6 +148,11 @@ void recibirCambioDeProcesoActivo(int cpu_socket, int* pidActivo){
 	if (recv(cpu_socket, &pid, sizeof(int32_t), 0) == -1) {
 		perror("recv");
 		exit(1);
+	}
+	tabla_de_paginas* tablaDePaginas = buscarPorPID(pidActivo);
+	if(tablaDePaginas==NULL){
+		enviarFAIL(cpu_socket, PID_NO_EXISTE);
+		return;
 	}
 	*pidActivo = pid;
 }
