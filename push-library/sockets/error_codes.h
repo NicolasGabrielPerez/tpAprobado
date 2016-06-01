0bc643959930a66dc3b7297b569b2b03d91b9334
martinvergara26 <martin.vergara@tictapps.com>
2016-06-01 20:25:45 -0300

    Archivo de codigo de errores

---
+++ push-library/sockets/error_codes.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_CODES_H_	extern int32_t PAGINA_NO_EXISTE;
+#define ERROR_CODES_H_
+
+#include <stdlib.h>
+
+	extern int32_t ESPACIO_NO_DISPONIBLE;
+	extern int32_t PID_YA_EXISTE;
+	extern int32_t PID_NO_EXISTE;
+	extern int32_t PAGINA_NO_EXISTE;
+#endif
