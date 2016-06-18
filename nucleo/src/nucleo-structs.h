3daa051d097c9e8bff724e0cedd4943308dc1a88
RamiroAg <ramiro.a.gutierrez@gmail.com>
2016-06-18 19:00:49 -0300

    Cambios realizados en núcleo, serialización y estructuras

--- nucleo/src/nucleo-structs.h
+++ nucleo/src/nucleo-structs.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <commons/config.h>
 #include <commons/collections/list.h>
+#include <commons/collections/queue.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -15,6 +16,15 @@
 #include <sockets/sockets.h>
 #include <sockets/config.h>
 
+extern int32_t memoryPageSize;
+
 	void initNucleo(t_config* config);
+
+	//Estructura que representa a un dispositivo de Entrada/Salida
+	typedef struct IO_Device{
+		char* ioId;
+		int sleepTime;
+		t_queue* BlockedProcessesQueue;
+	} IO_Device;
 
 #endif
