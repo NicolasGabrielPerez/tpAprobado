e80f158b9386009dbbecdb1959800664122ead49
asdfasd <sadasd@gasdmas.com>
2016-07-29 15:13:13 -0300

    Terminado deploy

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# tp-2016-1c-Pusheo-luego-existo...
+Deploy
 
-TEST - Christian Monasterios 
+1- Bajar el repo.
+2- Usando el nano, agregar al archivo .bashrc en la ultima linea, lo siguiente:
 
+export LD_LIBRARY_PATH=/home/utnso/tp-2016-1c-Pusheo-luego-existo.../push-library/Debug:$LD_LIBRARY_PATH
+
+3- verificar que se halla agregado correctamente
+echo $LD_LIBRARY_PATH
+
+3- sudo chmod +x compile.sh
+4- Correr el bash compile.sh
+5- Armar archivos de configuracion.
+6- Rezar
--- nucleo/Debug/src/subdir.mk
+++ nucleo/Debug/src/subdir.mk
@@ -7,6 +7,7 @@ C_SRCS += \
 ../src/consola-interfaz.c \
 ../src/cpu-interfaz.c \
 ../src/cpu.c \
+../src/inotify.c \
 ../src/io-device.c \
 ../src/nucleo-structs.c \
 ../src/nucleo.c \
@@ -18,6 +19,7 @@ OBJS += \
 ./src/consola-interfaz.o \
 ./src/cpu-interfaz.o \
 ./src/cpu.o \
+./src/inotify.o \
 ./src/io-device.o \
 ./src/nucleo-structs.o \
 ./src/nucleo.o \
@@ -29,6 +31,7 @@ C_DEPS += \
 ./src/consola-interfaz.d \
 ./src/cpu-interfaz.d \
 ./src/cpu.d \
+./src/inotify.d \
 ./src/io-device.d \
 ./src/nucleo-structs.d \
 ./src/nucleo.d \
@@ -41,7 +44,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2016-1c-Pusheo-luego-existo.../push-library" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/tp-2016-1c-Pusheo-luego-existo.../push-library" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
